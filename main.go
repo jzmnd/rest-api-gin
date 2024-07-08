78e1fec136970d0549d54562fa2ca86dcaa8d61e
Jeremy Smith <j.smith.03@cantab.net>
2024-07-08 16:54:12 -0500

    Added database connection

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // album represents data about a record album.
@@ -21,6 +27,13 @@ var albums = []Album{
 }
 
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	dbpool := dbConnect(ctx)
+	defer dbpool.Close()
+	log.Println("Connected to database")
+
 	router := gin.Default()
 	router.GET("/ping", handlePing)
 	router.GET("/albums", getAlbums)
@@ -30,42 +43,64 @@ func main() {
 	router.Run("0.0.0.0:8080")
 }
 
+// dbConnect creates the database connection.
+func dbConnect(c context.Context) *pgxpool.Pool {
+	config, err := pgxpool.ParseConfig("postgres://")
+	if err != nil {
+		log.Fatalf("Unable to parse config: %v\n", err)
+	}
+
+	// Update config using database parameters from the environment.
+	config.ConnConfig.Host = os.Getenv("DB_HOST")
+	config.ConnConfig.User = os.Getenv("DB_USER")
+	config.ConnConfig.Password = os.Getenv("DB_PASSWORD")
+	config.ConnConfig.Database = os.Getenv("DB_NAME")
+
+	// Create the database connection pool.
+	dbpool, err := pgxpool.NewWithConfig(c, config)
+	if err != nil {
+		log.Fatalf("Unable to create connection pool: %v\n", err)
+	}
+
+	return dbpool
+}
+
 // handlePing responds with a healthcheck message.
-func handlePing(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "ok"})
+func handlePing(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "ok"})
 }
 
 // getAlbums responds with the list of all albums as JSON.
-func getAlbums(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, albums)
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
 
 // postAlbums adds an album from JSON received in the request body.
-func postAlbums(context *gin.Context) {
+func postAlbums(c *gin.Context) {
 	var newAlbum Album
 
 	// Call BindJSON to bind the received JSON to newAlbum.
-	if err := context.BindJSON(&newAlbum); err != nil {
+	if err := c.BindJSON(&newAlbum); err != nil {
 		return
 	}
 
 	// Add the new album to the slice.
 	albums = append(albums, newAlbum)
-	context.IndentedJSON(http.StatusCreated, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
 // getAlbumByID locates the album whose ID value matches the id
 // parameter sent by the client, then returns that album as a response.
-func getAlbumByID(context *gin.Context) {
-	id := context.Param("id")
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
 
 	// Loop over the list of albums, looking for
 	// an album whose ID value matches the parameter.
 	for _, a := range albums {
 		if a.ID == id {
-			context.IndentedJSON(http.StatusOK, a)
+			c.IndentedJSON(http.StatusOK, a)
 			return
 		}
 	}
-	context.JSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	c.JSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
